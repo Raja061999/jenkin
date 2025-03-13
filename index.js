38bc85546f20ecc35920380009702de63222c4de
raja <raja@cloudrevelinnovation.com>
2025-03-13 23:08:06 +0530

    docker file update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello world , this is from raja and we are going to learn jenkin");
+    res.send("hello world , this is from raja and we are going to learn jenkin and it automated");
 });
 
 const PORT = 3003;
