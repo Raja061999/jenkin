9787650c31a8e11155c5a31c3364f68866e3b083
raja <raja@cloudrevelinnovation.com>
2025-03-14 19:09:42 +0530

    docker file update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello world this is");
+    res.send("hello world this is aws");
 });
 
 const PORT = 3003;
